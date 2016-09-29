03c7682c2f3b3eebd8de01f754d811be9e9bf984
Angel Chen & Thomas Graves <pair+angel-chen.thomaslogangraves@devbootcamp.com>
2016-09-29 17:59:56 -0400

    More parser work.

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -9,15 +9,20 @@ require 'pry'
     uri = URI.parse(url)
 
     params = {
-      "styleId" => input,
+      "name" => "Stout",
+      "randomCount" => input,
       "key" => "90e0fc85a2f2ced0c36502a13361d981"
     }
     uri.query = URI.encode_www_form(params)
 
     response = Net::HTTP.get(uri)
-    binding.pry
-     puts JSON.parse(response)
+    # binding.pry
+    JSON.parse(response)
   end
 end
 
-Parser.parse(15)
+categories = Parser.parse(1)
+# beers needs name, abv, ibu, glasswareId, srmId, availableId, styleId, ids
+# categories["data"].each {|hash| hash.select {|key, value| puts value if key == "name"}}
+categories["data"].each {|hash| hash.select {|key, value| puts value if key == "name"}}
+# categories.data.each {|category| puts category.name}
